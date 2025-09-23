486a774d9dfeb952692e6de4f60ee20263f9948a
jvvenkateshgh <jvvenkatesh80@gmail.com>
2025-09-23 13:37:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,16 @@ app.get('/api/get',(req,res) => {
     res.send({message:"hello back to nodejs"})
 })
 
+app.get('/api/get_user_details',(req,res) => {
+    res.send({
+        user:{
+            name:"satyam",
+            age:22,
+            contact:12345
+        }
+    })
+})
+
 app.listen(8000,() => {
     console.log("listing to 8000");
 })
